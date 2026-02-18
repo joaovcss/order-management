c18bff955d59f638c782f32bdb6e2379a333b3a6
João Victor <joaovictorcarvalhosilva29@gmail.com>
2026-02-18 12:03:20 -0300

    refactor: add global exception handling for API errors

--- src/main/java/com/yonix/order_management/infra/RestExceptionHandler.java
+++ src/main/java/com/yonix/order_management/infra/RestExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.yonix.order_management.infra;
 
 import com.yonix.order_management.exceptions.OrderExceptions.*;
+import com.yonix.order_management.exceptions.ProductExceptions.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -14,4 +17,19 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     private ResponseEntity<String> orderStatusException(OrderStatusException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    private ResponseEntity<String> productNotFoundException(ProductNotFoundException exception){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    private ResponseEntity<String> methodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception){
+        return ResponseEntity
+                .badRequest()
+                .body("invalid id format");
+    }
+
 }
