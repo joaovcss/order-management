0460239bece17e73db22d4e3e9480d4eadc0f1a0
João Victor <joaovictorcarvalhosilva29@gmail.com>
2026-02-24 20:21:02 -0300

    feat: creates static function to update an product in the entity

--- src/main/java/com/yonix/order_management/entity/Product.java
+++ src/main/java/com/yonix/order_management/entity/Product.java
@@ -1,7 +1,9 @@
 package com.yonix.order_management.entity;
 
 import com.yonix.order_management.dto.request.CreateProductRequest;
+import com.yonix.order_management.dto.request.UpdateProductRequest;
 import com.yonix.order_management.exceptions.ProductExceptions.InsufficientStockException;
+import com.yonix.order_management.exceptions.ProductExceptions.UpdateProductException;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -39,6 +41,28 @@ public class Product {
         product.description = request.description();
         product.price = request.price();
         product.stock = request.stock();
+        return product;
+    }
+
+    public static Product update(Product product,UpdateProductRequest request) {
+        if(request == null){
+            throw new UpdateProductException("the request cannot be null");
+        }
+        if(product == null) {
+            throw new UpdateProductException("the product to update cannot be null");
+        }
+        if(request.name() != null){
+            product.name = request.name();
+        }
+        if(request.description() != null){
+            product.description = request.description();
+        }
+        if(request.price() != null){
+            product.price = request.price();
+        }
+        if(request.stock() != null){
+            product.stock = request.stock();
+        }
         return product;
     }
 
