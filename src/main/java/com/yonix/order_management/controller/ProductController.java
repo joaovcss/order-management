31c2190679d9fa058923e228b0ec5e76acee40a5
João Victor <joaovictorcarvalhosilva29@gmail.com>
2026-02-24 20:23:52 -0300

    feat: creates PATCH method to update an product

--- src/main/java/com/yonix/order_management/controller/ProductController.java
+++ src/main/java/com/yonix/order_management/controller/ProductController.java
@@ -2,9 +2,11 @@ package com.yonix.order_management.controller;
 
 import com.yonix.order_management.dto.mapper.ProductMapper;
 import com.yonix.order_management.dto.request.CreateProductRequest;
+import com.yonix.order_management.dto.request.UpdateProductRequest;
 import com.yonix.order_management.dto.response.ProductResponse;
 import com.yonix.order_management.entity.Product;
 import com.yonix.order_management.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -40,6 +42,12 @@ public class ProductController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(ProductMapper.toProductResponse(product));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ProductResponse> update(@PathVariable UUID id, @RequestBody UpdateProductRequest request){
+        Product product = productService.updateProduct(id, request);
+        return ResponseEntity.ok(ProductMapper.toProductResponse(product));
     }
 
     @DeleteMapping("/{id}")
