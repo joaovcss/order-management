8316ae7de53079a4052ca43a2a9b401fbf7ba01c
João Victor <joaovictorcarvalhosilva29@gmail.com>
2026-03-12 19:47:53 -0300

    refactor: remove role parameter from RegisterRequestDTO

--- src/main/java/com/yonix/order_management/dto/request/RegisterRequest.java
+++ src/main/java/com/yonix/order_management/dto/request/RegisterRequest.java
@@ -3,5 +3,5 @@ package com.yonix.order_management.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record RegisterRequest(@NotNull @NotBlank String name, @NotNull @NotBlank String login, @NotNull @NotBlank String password, @NotNull @NotBlank String role) {
+public record RegisterRequest(@NotNull @NotBlank String name, @NotNull @NotBlank String login, @NotNull @NotBlank String password) {
 }
