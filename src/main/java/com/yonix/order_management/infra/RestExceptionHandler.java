7ad168d609c9d0421a919107be11b24d8668466a
João Victor <joaovictorcarvalhosilva29@gmail.com>
2026-03-12 19:52:59 -0300

    feat: add global exception handlers for orders, products and users

--- src/main/java/com/yonix/order_management/infra/RestExceptionHandler.java
+++ src/main/java/com/yonix/order_management/infra/RestExceptionHandler.java
@@ -2,9 +2,11 @@ package com.yonix.order_management.infra;
 
 import com.yonix.order_management.exceptions.OrderExceptions.*;
 import com.yonix.order_management.exceptions.ProductExceptions.ProductNotFoundException;
+import com.yonix.order_management.exceptions.ProductExceptions.InsufficientStockException;
+import com.yonix.order_management.exceptions.ProductExceptions.UpdateProductException;
+import com.yonix.order_management.exceptions.UserExceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -32,4 +34,38 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                 .body("invalid id format");
     }
 
+    @ExceptionHandler(OrderNotFoundException.class)
+    private ResponseEntity<String> orderNotFoundException(OrderNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler({OrderCancelledException.class, OrderPaidException.class, OrderShippedException.class, OrderDeliveredException.class})
+    private ResponseEntity<String> orderStateException(RuntimeException exception) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(InsufficientStockException.class)
+    private ResponseEntity<String> insufficientStockException(InsufficientStockException exception) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(UpdateProductException.class)
+    private ResponseEntity<String> updateProductException(UpdateProductException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    private ResponseEntity<String> userNotFoundException(UserNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
 }
